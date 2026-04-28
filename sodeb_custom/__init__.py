6203a858187e3844c3685e70d079e9f74f59473a
SOdeb <dstm.tomy@gmail.com>
2026-04-28 13:49:09 +0200

    update

--- sodeb_custom/__init__.py
+++ sodeb_custom/__init__.py
@@ -1,18 +1,28 @@
-"""The sodeb_custom integration."""
+"""
+The "hello world" custom component.
 
+This component implements the bare minimum that a component should implement.
+
+Configuration:
+
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+
+hello_world:
+"""
 from __future__ import annotations
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 # The domain of your component. Should be equal to the name of your component.
-DOMAIN = "sodeb_custom"
+DOMAIN = "hello_world"
 
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.set("sodeb_custom.test", "Works!")
+    hass.states.set('hello_world.Hello_World', 'Works!')
 
     # Return boolean to indicate that initialization was successfully.
     return True
